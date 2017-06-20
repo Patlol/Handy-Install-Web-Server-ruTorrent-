45258438e518dc65680618d6ab22d1ccc6df7855
patlol <pbs.olivier@free.fr>
2017-06-20 13:18:23 +0200

    Complément localisation utf-8 pour car spéciaux

--- HiwsT.sh
+++ HiwsT.sh
@@ -231,28 +231,38 @@ clear
 # localisation et infos système, 1ère passe
 if [ ! -e $REPLANCE"/firstusers" ]; then  # 1ère passe
 	# vérifie la localisation (pour debian) et modif
-
 	# if [[ ! `cat /etc/locale.gen | egrep ^[a-z].*UTF-8$` ]]; then
 	# if [[ ! `echo "é" | xxd |  awk -F" " '{ print $2 }'` != "c3a9"]]; then
+	# é return => é: command not found => ok \udcc3\udca9:  command not found pas ok
 	# export LC_ALL=en_US.UTF-8; unset LC_ALL # a la fin + rétablir les valeurs d'origine ?
 	#
-	if [[ ! `cat /etc/locale.gen | grep ^fr.*` ]]; then
-		echo "Votre systeme a besoin d'etre localise en $LANG"
-		sed -i -e "s/# $LANG UTF-8/$LANG UTF-8/" /etc/locale.gen && \
-    dpkg-reconfigure --frontend=noninteractive locales && \
-    update-locale LANG=$LANG && locale-gen \
-		&& {
-			echo
-			echo -e "Votre systeme va rebooter. Reconnectez-vous et\nrelancez le script"
-	    sleep 2
-			reboot
-		} \
-		|| {
-			echo
-			echo "Probleme avec la localisation en $LANG"
-			exit 1
-		}
-	fi
+	# if [[ ! `cat /etc/locale.gen | grep ^fr.*` ]]; then
+	# 	echo "Votre systeme a besoin d'etre localise en $LANG"
+	# 	sed -i -e "s/# $LANG UTF-8/$LANG UTF-8/" /etc/locale.gen && \
+  #   dpkg-reconfigure --frontend=noninteractive locales && \
+  #   update-locale LANG=$LANG && locale-gen \
+	# 	&& {
+	# 		echo
+	# 		echo -e "Votre systeme va rebooter. Reconnectez-vous et\nrelancez le script"
+	#     sleep 2
+	# 		reboot
+	# 	} \
+	# 	|| {
+	# 		echo
+	# 		echo "Probleme avec la localisation en $LANG"
+	# 		exit 1
+	# 	}
+	# fi
+	# Complèter la localisation (vps)
+	lang=$(cat /etc/locale.gen | egrep ^[a-z].*UTF-8$ | awk -F" " '{print $1 }')
+	export LANGUAGE=$lang
+	export LANG=$lang
+	export LC_ALL=$lang
+	update-locale LANGUAGE=$lang
+	update-locale LANG=$lang
+	update-locale LC_ALL=$lang
+	dpkg-reconfigure --frontend=noninteractive locales
+	locale-gen
 
 	# installe dialog si pas installé
 	apt-get update
